1d39391a431e9877cda78645c9620e0179382a86
Tristan <tristan@rubadeau.com>
2014-03-13 04:27:50 +0000

    Add body to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 book_1 = Book.create name: "Cooking", pages: 199, summary: "Home-cooking"
-["Dan", "Tim", "Gary"].each do |name|  
-  book_1.reviews.create name: name, rating: 4, body: "Great Read"
+["Dan", "Tim", "Gary", "Tom", "Hank"].each do |name|  
+  ["Great Read", "Interesting", "okey", "could have been better", "lame"].each do |body|
+    book_1.reviews.create name: name, rating: 4, body: body
+end
 end
